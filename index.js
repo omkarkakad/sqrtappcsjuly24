c623bc20c1ecfeae1a7a336c143e7ce017ac9be3
omkarkakad <139750149+omkarkakad@users.noreply.github.com>
2024-07-14 10:19:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const express=require("express");
+const cors=require("cors");
+const fs=require("fs");
+const port=process.env.PORT || 9000;
+
+const app=express();
+app.use(cors());
+
+app.get("/compute",(req,res)=>{
+	let num=req.query.number;
+	let n=parseFloat(num);
+	let r=n**0.5;
+	let msg="Square root of " + n + " is " + r.toFixed(2);
+
+	let logdata=msg + " " + new Date().toString() + "\n";
+	fs.appendFile("result.txt",logdata,(err)=>{
+		if (err)	
+			console.log(err);
+
+	});
+	res.json({"msg":msg});
+
+
+});
+
+
+
+app.listen(port, () =>{console.log("ready @" + port);});
